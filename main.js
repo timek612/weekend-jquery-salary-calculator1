4cb0fc81720d413421e658eefb05745a176eb551
AJ Timek <Timek612@gmail.com>
2022-07-23 15:02:35 -0500

    finished base

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 console.log('js');
 $(readyNow);
 
+let totalYearly = 0;
+let salaries = [];
+
+
 function readyNow() {
     console.log('JQ');
-    $('#submit').on('click', handleClick)
+    $('#submit').on('click', handleClick);
+    //$('#submit').on('click', calculateRemainingBudget);
+    $('#tableBod').on('click', '.delete', deleteLine);
+
 }
 
 function handleClick() {
@@ -14,11 +21,13 @@ function handleClick() {
     let title = $('#title').val();
     let salary = $('#annualSal').val();
 
-    console.log(fName);
-    console.log(lName);
-    console.log(idNumber);
-    console.log(title);
-    console.log(salary);
+    // console.log(fName);
+    // console.log(lName);
+    // console.log(idNumber);
+    // console.log(title);
+    // console.log(salary);
+
+    calculateRemainingBudget();
 
     $('#firstName').val('');
     $('#lastName').val('');
@@ -28,26 +37,65 @@ function handleClick() {
 
     $('#tableBod').append(`
     <tr>
-    <td id="fName1">` + fName + `</td>
-    <td id="lName1">` + lName + `</td>
-    <td id="ID1">` + idNumber + `</td>
-    <td id="title1">` + title + `</td>
-    <td id="sal1">` + salary + `</td>
-    <td><button id="delete1">Delete</button></td>
+    <td>${fName}</td>
+    <td>${lName}</td>
+    <td>${idNumber}</td>
+    <td>${title}</td>
+    <td>${salary}</td>
+    <td><button class="delete">Delete</button></td> 
     </tr>
     `)
-
-    $('#delete1').on('click', deleteLine)
+    // ^^ button is created when the page is loaded, so we need a dynamic click listener, line7.
+    
 
 }
 
 function deleteLine() {
     console.log('in delete line');
-    $('#fName1').remove();
-    $('#lName1').remove();
-    $('#ID1').remove();
-    $('#title1').remove();
-    $('#sal1').remove();
-    $('#delete1').remove();
+    $(this).closest('tr').remove();
+    removeSalary();
+    // $('#fName1').remove();
+    // $('#lName1').remove();
+    // $('#ID1').remove();
+    // $('#title1').remove();
+    // $('#sal1').remove();
+    // $('#delete1').remove();
 
+}
+
+
+function calculateRemainingBudget() {
+    console.log('in remaining budget');
+    let variable = 0;
+    let salary = parseInt($('#annualSal').val());
+    //parseInt(salary); why tf didn't this work
+    salaries.push(salary);
+    //console.log(salary);
+    for (let i = 0; i < salaries.length; i++) {
+        variable += salaries[i];
+        //console.log(variable);
+    }    
+    totalYearly = 0;
+    totalYearly += variable;
+    console.log(totalYearly);
+    let totalMonthly = totalYearly /= 12;
+    let el = $('#monthlyBudget');
+    el.empty();
+    el.append(totalMonthly);
+
+    // this is pretty messy, if extra time, clean up.
+    
+    if (totalMonthly > 20000) {
+        document.getElementById("monthly").style.color = "red";
+        $('footer').append (alert(`
+            WARNING!! WARNING!!
+            YOU HAVE EXCEEDED THE $20,000 LIMIT
+            LOWER COSTS OR FACE DEATH BY FIRE!`)); 
+        
+    }
+    
+}
+
+function removeSalary() {
+    
 }
